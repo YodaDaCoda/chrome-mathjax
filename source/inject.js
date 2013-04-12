20f9e21fd6635f9ceb3da485e7d5e637ad58379c
William Pickering <pickeringw+github@gmail.com>
2013-04-12 20:06:38 +1000

    Initial commit. Works well.

---
+++ source/inject.js
@@ -0,0 +1,5 @@
+var script = document.createElement("script");
+script.onload = function(){console.log("mathjax loaded");};
+script.type = "text/javascript";
+script.src = "http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-MML-AM_HTMLorMML";
+document.getElementsByTagName('head')[0].appendChild(script);
