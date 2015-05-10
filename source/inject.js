740df55c0e4a8123f5bce85b9f4968b0d6ba5fa7
William Pickering <pickeringw+github@gmail.com>
2015-05-10 16:11:53 +1000

    Use https on https pages

--- source/inject.js
+++ source/inject.js
@@ -1,5 +1,5 @@
 var script = document.createElement("script");
 script.onload = function(){console.log("mathjax loaded");};
 script.type = "text/javascript";
-script.src = "http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-MML-AM_HTMLorMML";
+script.src = "//cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-MML-AM_HTMLorMML";
 document.getElementsByTagName('head')[0].appendChild(script);
